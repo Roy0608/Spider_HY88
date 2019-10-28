bfd71f7375aad3103c3e72fd1b36597c0e070e1f
Roy <pinchen.pan@tendata.cn>
2019-10-28 10:45:56 +0800

    v 1.2 p_c_r

--- HY88/spiders/HY.py
+++ HY88/spiders/HY.py
@@ -20,7 +20,29 @@ class HySpider(scrapy.Spider):
         reg_urls = response.xpath('//div[@id="subarealist"]/div[2]/a/@href').extract() # 区县url
         for reg in reg_urls:
             print(reg)
+            yield scrapy.Request(
+                reg,
+                callback=self.parse_ind_urls  # 不同行业和所属省市区
+            )
+
+    def parse_ind_urls(self, response):
+        item = {}
+        # 省
+        item['pro'] = response.xpath('//div[@class="subNav"]/a[2]/text()').extract()[0][:-4]
+        # 市
+        item['city'] = response.xpath('//div[@class="subNav"]/a[3]/text()').extract()[0][:-4]
+        # 区
+        item['reg'] = response.xpath('//div[@class="subNav"]/text()')\
+                                            .extract()[2].replace(' » ','').replace('\r\n','')
+
+        ind_urls = response.xpath('//div[@class="tag_tx"]/ul/li/a/@href').extract() # 行业url
+        for ind in ind_urls:
+            print(ind)
+            print(item)
             # yield scrapy.Request(
-            #     reg,
-            #     callback=self.parse_ind_urls  # 不同行业和所属省市区
+            #     ind,
+            #     callback=self.parse_ind_cont,  # 企业信息
+            #     meta={'item': deepcopy(item)},
+            #     dont_filter=True
             # )
+
