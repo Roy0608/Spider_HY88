faa3b961fcbff7906c0fda30ff560702a7455059
Roy <pinchen.pan@tendata.cn>
2019-10-28 10:39:26 +0800

    v 1.0 region

--- HY88/spiders/HY.py
+++ HY88/spiders/HY.py
@@ -11,7 +11,16 @@ class HySpider(scrapy.Spider):
         city_urls = response.xpath('//dl[@id="clist"]/dd/a/@href').extract()  # 城市url
         for city in city_urls:
             print(city)
+            yield scrapy.Request(
+                city,
+                callback=self.parse_reg_urls  # 市所属的区县
+            )
+
+    def parse_reg_urls(self, response):
+        reg_urls = response.xpath('//div[@id="subarealist"]/div[2]/a/@href').extract() # 区县url
+        for reg in reg_urls:
+            print(reg)
             # yield scrapy.Request(
-            #     city,
-            #     callback=self.parse_reg_urls  # 市所属的区县
+            #     reg,
+            #     callback=self.parse_ind_urls  # 不同行业和所属省市区
             # )
