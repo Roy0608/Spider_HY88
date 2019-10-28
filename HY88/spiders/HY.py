621e0a59f66b1da3ef44a048c4aa2126c0ee971a
Roy <pinchen.pan@tendata.cn>
2019-10-28 10:33:01 +0800

    v 1.0 city

--- HY88/spiders/HY.py
+++ HY88/spiders/HY.py
@@ -3,9 +3,15 @@ import scrapy
 
 
 class HySpider(scrapy.Spider):
-    name = 'HY'
-    allowed_domains = ['huangye88.com']
-    start_urls = ['http://huangye88.com/']
+    name = 'HY'  # 爬虫名
+    allowed_domains = ['huangye88.com']  # 域名
+    start_urls = ['http://b2b.huangye88.com/region/']  #起始地址
 
     def parse(self, response):
-        pass
+        city_urls = response.xpath('//dl[@id="clist"]/dd/a/@href').extract()  # 城市url
+        for city in city_urls:
+            print(city)
+            # yield scrapy.Request(
+            #     city,
+            #     callback=self.parse_reg_urls  # 市所属的区县
+            # )
